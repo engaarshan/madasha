af7c3e0cc521606f8de4cbf85828fad94f9e52c9
aarshan <abdullahiaarshan@gmail.com>
2021-12-20 11:56:18 +0300

    welcome screen design done

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+
+
+import AppButton from '../components/AppButton';
+import AppText from '../components/AppText';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+            blurRadius={5}
             style={styles.background}
             source={require("../assets/background.jpg")}
             >
-        <View style={styles.logoContainer}>
-            <Image style={styles.logo} source={require("../assets/logo-red.png")} />  
-            <Text>Join the community of innovations</Text>   
-        </View>  
-        <View style={styles.loginButton}></View>
-        <View style={styles.resgisterButton}></View>
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require("../assets/logo.png")} />
+                <AppText>Join the community of innovations</AppText>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login" onPress={() => console.log("Login clicked")}></AppButton>
+                <AppButton title="Register" color="secondary" onPress={() => console.log("Register clicked")}></AppButton> 
+            </View>
         </ImageBackground>
     );
 }
@@ -23,26 +30,31 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center"
     },
-    loginButton:{
-        width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65"
+    buttonsContainer: {
+        padding: 20,
+        width: '100%'
     },
-
     logoContainer:{
         position:"absolute",
         top: 70,
         alignItems:'center'
     },
     logo:{
-        width: 100,
-        height: 100,
+        width: 180,
+        height: 180,
+    },
+    loginButton:{
+        width: "100%",
+        height: 70,
+        backgroundColor: "#fc5c65",
+        marginBottom: 50
     },
     
     resgisterButton:{
         width: "100%",
         height: 70,
-        backgroundColor: "#4ecdcf"
+        backgroundColor: "#4ecdcf",
+        marginBottom: 50
     }
 })
 
