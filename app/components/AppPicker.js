d4d815e10e065899d1bde99926613bd5d0741c70
aarshan <abdullahiaarshan@gmail.com>
2021-12-22 11:46:00 +0300

    Additional Improvements

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -14,13 +14,22 @@ import Screen from "./AppScreen";
 import defaultStyles from "../config/styles";
 import PickerItem from "./PickerItem";
 
-function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
+function AppPicker({
+  icon,
+  items,
+  numberOfColumns = 1,
+  onSelectItem,
+  width = "100%",
+  PickerItemComponent = PickerItem,
+  placeholder,
+  selectedItem,
+}) {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <>
       <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
-        <View style={styles.container}>
+        <View style={[styles.container, { width }]}>
           {icon && (
             <MaterialCommunityIcons
               name={icon}
@@ -47,9 +56,11 @@ function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
           <Button title="Close" onPress={() => setModalVisible(false)} />
           <FlatList
             data={items}
+            numColumns={numberOfColumns}
             keyExtractor={(item) => item.value.toString()}
             renderItem={({ item }) => (
-              <PickerItem
+              <PickerItemComponent
+                item={item}
                 label={item.label}
                 onPress={() => {
                   setModalVisible(false);
@@ -69,7 +80,6 @@ const styles = StyleSheet.create({
     backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
-    width: "100%",
     padding: 15,
     marginVertical: 10,
   },
--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -4,15 +4,25 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, placeholder }) {
+function AppFormPicker({
+  items,
+  numberOfColumns,
+  name,
+  width,
+  PickerItemComponent,
+  placeholder,
+}) {
   const { errors, setFieldValue, touched, values } = useFormikContext();
 
   return (
     <>
       <AppPicker
         items={items}
+        numberOfColumns={numberOfColumns}
         onSelectItem={(item) => setFieldValue(name, item)}
         placeholder={placeholder}
+        width={width}
+        PickerItemComponent={PickerItemComponent}
         selectedItem={values[name]}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
